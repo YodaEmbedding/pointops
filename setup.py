ac373cb83eaf2c932ed95d6f68401a8913af4fef
Mateen Ulhaq <mulhaq2005@gmail.com>
2024-01-31 22:04:20 -0800

    build: pyproject.toml, setup.py

--- setup.py
+++ setup.py
@@ -4,30 +4,32 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
     name='pointops',
+    install_requires=[
+        "numpy",
+        "torch",
+    ],
     ext_modules=[
         CUDAExtension('pointops_cuda', [
-            'src/pointops_api.cpp',
+            'pointops/src/pointops_api.cpp',
 
-            'src/ballquery/ballquery_cuda.cpp',
-            'src/ballquery/ballquery_cuda_kernel.cu',
-            'src/knnquery/knnquery_cuda.cpp',
-            'src/knnquery/knnquery_cuda_kernel.cu',
-            'src/knnquery_heap/knnquery_heap_cuda.cpp',
-            'src/knnquery_heap/knnquery_heap_cuda_kernel.cu',
-            'src/grouping/grouping_cuda.cpp',
-            'src/grouping/grouping_cuda_kernel.cu',
-            'src/grouping_int/grouping_int_cuda.cpp',
-            'src/grouping_int/grouping_int_cuda_kernel.cu',
-            'src/interpolation/interpolation_cuda.cpp',
-            'src/interpolation/interpolation_cuda_kernel.cu',
-            'src/sampling/sampling_cuda.cpp',
-            'src/sampling/sampling_cuda_kernel.cu',
+            'pointops/src/ballquery/ballquery_cuda.cpp',
+            'pointops/src/ballquery/ballquery_cuda_kernel.cu',
+            'pointops/src/knnquery/knnquery_cuda.cpp',
+            'pointops/src/knnquery/knnquery_cuda_kernel.cu',
+            'pointops/src/grouping/grouping_cuda.cpp',
+            'pointops/src/grouping/grouping_cuda_kernel.cu',
+            'pointops/src/grouping_int/grouping_int_cuda.cpp',
+            'pointops/src/grouping_int/grouping_int_cuda_kernel.cu',
+            'pointops/src/interpolation/interpolation_cuda.cpp',
+            'pointops/src/interpolation/interpolation_cuda_kernel.cu',
+            'pointops/src/sampling/sampling_cuda.cpp',
+            'pointops/src/sampling/sampling_cuda_kernel.cu',
 
-            'src/labelstat/labelstat_cuda.cpp',
-            'src/labelstat/labelstat_cuda_kernel.cu',
+            'pointops/src/labelstat/labelstat_cuda.cpp',
+            'pointops/src/labelstat/labelstat_cuda_kernel.cu',
 
-            'src/featuredistribute/featuredistribute_cuda.cpp',
-            'src/featuredistribute/featuredistribute_cuda_kernel.cu'
+            'pointops/src/featuredistribute/featuredistribute_cuda.cpp',
+            'pointops/src/featuredistribute/featuredistribute_cuda_kernel.cu'
         ],
                       extra_compile_args={'cxx': ['-g'],
                                           'nvcc': ['-O2']})
