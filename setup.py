dbdd5c5601ea63e2257025c662d394bce9e50474
Mateen Ulhaq <mulhaq2005@gmail.com>
2024-02-28 14:18:41 -0800

    chore(release): 0.0.0.post2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
     name="pointops-yoda",
-    version="0.0.0.post1",
+    version="0.0.0.post2",
     packages=find_packages(),
     install_requires=[
         "numpy",
